49e3c5e5f0cc3472d2da5d191c96a6386d65437e
daniele.falchetti2 <daniele.falchetti@viewnext.com>
2023-01-11 21:52:28 +0100

    s

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
+import json
 from data import expenses
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -9,10 +10,14 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/choose_expanses")
+@app.route("/choose_expanses", methods=["POST"])
 def choose_expanses():
-    data = expenses(groups=False, csv=False, month=1, year=2021)
-    return data
+    parameter = request.get_json()
+    groups = parameter["groups"]
+    csv = parameter["csv"]
+    month = parameter["month"]
+    year = parameter["year"]
+    return expenses(groups, csv, month, year)
 
 
 if __name__ == "__main__":
--- static/index.js
+++ static/index.js
@@ -1,14 +1,27 @@
 const app = document.querySelector("#app");
 const button = document.createElement("button");
 const container = document.createElement("div");
-const paragraph = document.createElement("p");
 button.textContent = "click";
+const parameter = {
+  groups: false,
+  csv: false,
+  month: null,
+  year: 2022,
+};
 button.addEventListener("click", () => {
-  fetch("/choose_expanses")
+  fetch("/choose_expanses", {
+    method: "POST",
+    credentials: "include",
+    body: JSON.stringify(parameter),
+    cache: "no-cache",
+    headers: new Headers({
+      "content-type": "application/json",
+    }),
+  })
     .then((res) => res.json())
     .then((data) => {
-      paragraph.textContent = JSON.stringify(data);
+      return data;
     });
 });
-container.append(button, paragraph);
+container.append(button);
 app.appendChild(container);
