107733dab8e9f8367bc44bd60e21e61ef00e496f
daniele.falchetti2 <daniele.falchetti@viewnext.com>
2023-01-11 19:10:21 +0100

    s

--- static/index.js
+++ static/index.js
@@ -1,13 +1,14 @@
 const app = document.querySelector("#app");
 const button = document.createElement("button");
 const container = document.createElement("div");
+const paragraph = document.createElement("p");
 button.textContent = "click";
 button.addEventListener("click", () => {
   fetch("/choose_expanses")
     .then((res) => res.json())
     .then((data) => {
-      container.textContent = data;
+      paragraph.textContent = JSON.stringify(data);
     });
 });
-container.appendChild(button);
+container.append(button, paragraph);
 app.appendChild(container);
