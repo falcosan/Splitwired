d649af47b759d85fbdcac54c26f64af3b2062f86
daniele.falchetti2 <daniele.falchetti@viewnext.com>
2023-01-24 17:59:39 +0100

    w

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-
+yarn
+yarn build
 pip install --upgrade pip
 pip install $(grep -vE "numpy|pandas" requirements.txt)
 pip install numpy==1.16.0
