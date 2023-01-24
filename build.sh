3044e1cf3ecabbac51fc24de5685680dc97b956e
daniele.falchetti2 <daniele.falchetti@viewnext.com>
2023-01-24 16:19:07 +0100

    w

--- build.sh
+++ build.sh
@@ -3,5 +3,7 @@
 set -o errexit
 
 pip install --upgrade pip
-pip install -r requirements.txt
+pip install -r requirements.txt --exclude-editable numpy pandas
+pip install numpy==1.16.0
+pip install pandas==1.3.5
 npm run build
