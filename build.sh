30afd4ec8e761e426ba31e07561c6fb71ed15497
daniele.falchetti2 <daniele.falchetti@viewnext.com>
2023-01-24 17:50:00 +0100

    w

--- build.sh
+++ build.sh
@@ -2,10 +2,6 @@
 # exit on error
 set -o errexit
 
-curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash -
-sudo apt-get install -y nodejs
-npm install
-npm run build
 pip install --upgrade pip
 pip install $(grep -vE "numpy|pandas" requirements.txt)
 pip install numpy==1.16.0
