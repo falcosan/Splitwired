a40b8b8783139cb4995757bb4419d5b11fe1ab7e
daniele.falchetti2 <daniele.falchetti@viewnext.com>
2023-02-01 01:25:17 +0100

    w

--- static/sw.js
+++ static/sw.js
@@ -1,5 +1,4 @@
 const PRECACHE = "precache";
-const RUNTIME = "runtime";
 const PRECACHE_URLS = [
   "static/index.html",
   "static/style.css",
@@ -7,47 +6,15 @@ const PRECACHE_URLS = [
 ];
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches
-      .open(PRECACHE)
-      .then((cache) => cache.addAll(PRECACHE_URLS))
-      .then(self.skipWaiting())
-  );
-});
-self.addEventListener("activate", (event) => {
-  const currentCaches = [PRECACHE, RUNTIME];
-  event.waitUntil(
-    caches
-      .keys()
-      .then((cacheNames) => {
-        return cacheNames.filter(
-          (cacheName) => !currentCaches.includes(cacheName)
-        );
-      })
-      .then((cachesToDelete) => {
-        return Promise.all(
-          cachesToDelete.map((cacheToDelete) => {
-            return caches.delete(cacheToDelete);
-          })
-        );
-      })
-      .then(() => self.clients.claim())
+    caches.open(PRECACHE).then((cache) => {
+      return cache.addAll(PRECACHE_URLS);
+    })
   );
 });
 self.addEventListener("fetch", (event) => {
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then((cachedResponse) => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-        return caches.open(RUNTIME).then((cache) => {
-          return fetch(event.request).then((response) => {
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
-      })
-    );
-  }
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
