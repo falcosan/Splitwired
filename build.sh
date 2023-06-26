cf02cfaeadd89ef1414195f57a71b747da6154a8
daniele.falchetti2 <daniele.falchetti@viewnext.com>
2023-06-26 14:47:02 +0200

    a

--- build.sh
+++ build.sh
@@ -5,7 +5,6 @@ set -o errexit
 yarn
 yarn build
 pip install --upgrade pip
-pip install $(grep -vE "numpy|pandas|Flask" requirements.txt)
+pip install $(grep -vE "numpy|pandas" requirements.txt)
 pip install numpy==1.16.0
 pip install pandas==1.3.5
-pip install Flask==2.2.3
